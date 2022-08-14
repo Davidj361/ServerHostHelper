5348b5ff65ccdb5761d5f30f0dcc4e833af050de
Davidj361 <david.j.361@gmail.com>
2022-08-14 15:21:07 -0400

    Added configGui, fixed emacs characters

--- Lua/ForcedAutorun/init.lua
+++ Lua/ForcedAutorun/init.lua
@@ -1,23 +1,33 @@
+local SHH = {}
 local modName = '[ServerHostHelper]'
 local hello = "Starting up!"
 local Log = function (message)
 	print(modName .. " " .. message)
 end
 
+-- local function mod.ToggleGUI()
+-- 	mod.GUIOpen = not mod.GUIOpen
+--
+--     if mod.GUIOpen then
+--         mod.ShowGUI()
+--     else
+--         mod.HideGUI()
+--     end
+-- end
 
 
 if CLIENT then
-	
-	if ServerHostHelperCl == nil then
-		ServerHostHelperCl = {}
+
+	if mod.cl == nil then
+		mod.cl = {}
 	else
 		return
 	end
-	
+
 	Log("[Client] "..hello)
 	LuaUserData.RegisterType("Barotrauma.GameMain")
 	local GameMain = LuaUserData.CreateStatic("Barotrauma.GameMain")
-	
+
 	Hook.Add("think", "logWindowHotkey", function()
 		if GUI.GUI.KeyboardDispatcher.Subscriber ~= nil then return end -- Avoid executing with chat focused
 		if PlayerInput.KeyHit(Keys.Z) then
@@ -25,29 +35,30 @@ if CLIENT then
 		end
 	end)
 
+
 elseif SERVER then
 
-	if ServerHostHelperSv == nil then
-		ServerHostHelperSv = {}
+	if mod.sv == nil then
+		mod.sv = {}
 	else
 		return
 	end
-	
+
 	Log("[Server] "..hello)
 	local counter = 0
 	local maxBackups = 5
 	local savePath = ""
 	LuaUserData.RegisterType("Barotrauma.SaveUtil")
 	local SaveUtil = LuaUserData.CreateStatic("Barotrauma.SaveUtil")
-	
-	
+
+
 	local function addBot()
 		local session = Game.GameSession
 		local crewManager = session.CrewManager
 		local chr = CharacterInfo("human")
 		chr.TeamID = CharacterTeamType.Team1
 		crewManager.AddCharacterInfo(chr)
-		
+
 		-- Taken from https://github.com/MassCraxx/MidRoundSpawn/blob/main/Lua/Autorun/midroundspawn.lua
 		local spawnWayPoints = WayPoint.SelectCrewSpawnPoints({chr}, Submarine.MainSub)
 		local randomIndex = Random.Range(1, #spawnWayPoints)
@@ -71,22 +82,22 @@ elseif SERVER then
 		end
 
 		-- none found, go random
-		if waypoint == nil then 
+		if waypoint == nil then
 			MidRoundSpawn.Log("WARN: No valid job waypoint found for " .. chr.Job.Name.Value .. " - using random")
 			waypoint = WayPoint.GetRandom(SpawnType.Human, nil, Submarine.MainSub)
 		end
 
-		if waypoint == nil then 
+		if waypoint == nil then
 			MidRoundSpawn.Log("ERROR: Could not spawn player - no valid waypoint found")
-			return false 
+			return false
 		end
-		
+
 		local character = Character.Create(chr, waypoint.WorldPosition, Game.NetLobbyScreen.LevelSeed)
 		character.GiveJobItems()
 		Log("Added bot")
 	end
-	
-	
+
+
 	local function removeBot()
 		for key, chara in pairs(Character.CharacterList) do
 			if chara.TeamID == CharacterTeamType.Team1 and chara.IsBot and not chara.IsDead then
@@ -96,8 +107,8 @@ elseif SERVER then
 		end
 		Log("Removed bot")
 	end
-	
-	
+
+
 	local function count()
 		Log("Counting...")
 		local nPly = 0
@@ -115,8 +126,8 @@ elseif SERVER then
 		Log("nBots = "..nBots)
 		return nPly+nBots
 	end
-	
-	
+
+
 	local function handleBots()
 		Log("Handling bots")
 		local n = count()
@@ -129,40 +140,46 @@ elseif SERVER then
 			end
 		end
 	end
-	
-	
+
+
 	local function backup()
 		Log("Backing up save...")
-		
+
 		local tmp = Game.GameSession.SavePath
 		if savePath ~= tmp then
 			savePath = tmp
 			counter = 0
 		end
-		
+
 		local basePath = string.match(savePath, "^.*\\")
 		local baseName = string.match(savePath, "\\([^\\]*)$")
 		baseName = string.match(baseName, "(.*)[.]")
-		
+
 		local saveFilename = baseName .. '-' .. counter .. '.save'
 		local xmlFilename = baseName .. '-' .. counter .. '_CharacterData.xml'
-		
+
 		--Game.SaveGame(basePath .. saveFilename) -- Doesn't always work
 		SaveUtil.SaveGame(basePath .. saveFilename)
 		Game.GameSession.Save(basePath .. xmlFilename)
-		
+
 		counter = (counter + 1) % maxBackups
 		Log("Save is backed up!")
 	end
-	
-	
+
+
 	-- Hooks
 	Hook.Add("client.connected", "handleBotsOnConnect", handleBots)
 	Hook.Add("client.disconnected", "handleBotsOnDisconnect", handleBots)
 	Hook.Add("roundStart", "handleBotsOnRoundStart", handleBots)
 	Hook.Add("roundEnd", "saveBackup", backup)
-	
-	
+
+
+	-- Commands
+	Game.AddCommand("shh", "Opens Server Host Helper's config", function ()
+		NT.ToggleGUI()
+	end)
+
+
 	--Hook.Add("chatMessage", "debugging", function(message)
 	--
 	--	print("message = "..message)
@@ -183,5 +200,5 @@ elseif SERVER then
 	--		for i=1,1 do print("i = "..i) end
 	--	end
 	--end)
-	
+
 end
